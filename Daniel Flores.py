718d01f93500b1f897e9c72fe3245b7bd94ab352
DanielFlores23 <49959778+DanielFlores23@users.noreply.github.com>
2019-05-28 07:21:03 -0700

    Add files via upload

---
+++ Daniel Flores.py
@@ -0,0 +1,15 @@
+numero_entero_p = 50
+print("yo soy un numero entero positivo", numero_entero_p)
+numero_entero_n = - 50
+print("yo soy un numero entero negativo", numero_entero_n)
+
+numero_flotante_p = 5.50
+print("yo soy un numero flotante positivo", numero_entero_p
+numero_flotante_n = -5.50
+print("yo soy un numero flotante negativo", numero_entero_n
+
+numero_doble_p = 70000000
+print("yo soy un numero doble positivo", numero_doble_p)
+numero_doble_n = - 70000000
+print("yo soy un numero doble negativo", numero_doble_n)
+      
---
+++ multiplicacion.py
@@ -0,0 +1,23 @@
+print("Table del 2")
+for elemento in range(1,11):
+    resultado = 2 * elemento
+    print("2 x {0}" = {1}".format(elemento,resultado))
+print()
+
+print("Table del 4")
+for elemento in range(1,11):
+    resultado = 4 * elemento
+    print("4 x {0}" = {1}".format(elemento,resultado))
+print()
+
+print("Table del 23")
+for elemento in range(1,11):
+    resultado = 23 * elemento
+    print("23 x {0}" = {1}".format(elemento,resultado))
+print()
+
+print("Table del 76")
+for elemento in range(1,11):
+    resultado = 76 * elemento
+    print("76 x {0}" = {1}".format(elemento,resultado))
+print()
